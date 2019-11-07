ac4e51aaf737e0265a3a72e1f56bc323b120eb49
spamlite2005 <spamlite2005@gmail.com>
2019-11-07 03:32:15 -0500

    filtered data!

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,27 +1,48 @@
 // from data.js
 var tableData = data;
-
+console.log(tableData);
 // YOUR CODE HERE!
-d3.select("table")
-    .select("tbody")
-    .select("#ufo-table")
-    .enter()
-    .append("tr")
-    .append("td")
-    .text(function(d) {
-    return `<td>${d.datetime}</td>
-            <td>${d.city}</td>
-            <td>${d.state}</td>
-            <td>${d.country}</td>
-            <td>${d.shape}</td>
-            <td>${d.durationMinutes}</td>
-            <td>${d.comments}</td>`;
-  });
 
-  // tabledata.forEach((data) => {
-  //   var row = tbody.append("tr");
-  //   Object.entries(weatherReport).forEach(([key, value]) => {
-  //     var cell = row.append("td");
-  //     cell.text(value);
-  //   });
-  // });
+// Create a functino to build the table
+ function buildTable(t) {
+  var tbody = d3.select("tbody");
+  var entryData = t.forEach(function(entry) {
+//    console.log(entry);
+    var row = tbody.append("tr");
+    var rowdata = Object.values(entry);
+//    console.log(rowdata);
+    var obj1 = rowdata.forEach(function(d,i) {
+      var cell = row.append("td");
+      cell.text(rowdata[i]);
+    });
+  });
+ };
+
+buildTable(tableData);
+//function filterTable()
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+  var emptyTable = d3.select("#ufo-table").select('tbody').selectAll('tr').remove();
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Confirm the input date
+  console.log(inputValue);
+  //console.log(tableData);
+
+  // Filter tableData based on user input
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  // Confirm filteredData Array
+  console.log(filteredData);
+
+  // Build the filteredData Table
+  buildTable(filteredData); 
+
+  });
